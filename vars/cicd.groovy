a3afc6792b49d1fd409681a5b5f68a02ede6f8a0
monu-19 <111163702+monu-19@users.noreply.github.com>
2022-08-20 15:46:34 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,5 +9,10 @@ def newMaven()
 }
 def newDeploy(ip,appname)
 {
-  
-  
+   deploy adapters: [tomcat9(credentialsId: '4a198297-7196-45d6-9475-249fcb77e483', path: '', url:"$(ip)"], contextPath:"$(appname)", war: '**/*.war
+}
+def newTest
+{
+  echo "$(jobname)"
+  sh"java -jar/home/ubuntu/.jenkins/workspace/$(jobname)/testing.jar"
+}
